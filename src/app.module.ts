da6283cb244e39f37cbc26c58cfdc6ce3d6a4d60
mrogers8912 <mrogers89@outlook.com>
2026-06-10 06:31:13 +0100

    imported AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+})
+export class AppModule {}
 
 @Module({
   imports: [AuthModule],
