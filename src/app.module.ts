e32db775f97d162458cb845500b2b21027bc86fa
mrogers8912 <mrogers89@outlook.com>
2026-06-10 06:34:20 +0100

    fix: install axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-})
-export class AppModule {}
-
-@Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
